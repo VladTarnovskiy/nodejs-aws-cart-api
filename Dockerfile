0704c507eef4e1472efd6a1b5630e9ad2bdec92c
Vladislav Tarnovskiy <v.arnovskiy@georank.ru>
2026-06-06 18:08:21 +0300

    ref: nest app deployment

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:20-alpine AS builder
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm ci
+
+COPY nest-cli.json tsconfig*.json ./
+COPY src ./src
+
+RUN npm run build && npm prune --omit=dev
+
+FROM public.ecr.aws/lambda/nodejs:20
+
+COPY --from=builder /app/dist ${LAMBDA_TASK_ROOT}
+COPY --from=builder /app/node_modules ${LAMBDA_TASK_ROOT}/node_modules
+
+CMD ["src/lambda.handler"]
--- cdk/lib/cart-api-stack.ts
+++ cdk/lib/cart-api-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import * as apigwv2 from 'aws-cdk-lib/aws-apigatewayv2';
 import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import * as path from 'path';
 
@@ -10,26 +9,18 @@ export class CartApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const cartApiLambda = new nodejs.NodejsFunction(this, 'CartApiLambda', {
-      functionName: 'cartApi',
-      runtime: lambda.Runtime.NODEJS_20_X,
-      entry: path.join(__dirname, '../../src/lambda.ts'),
-      handler: 'handler',
-      memorySize: 512,
-      timeout: cdk.Duration.seconds(30),
-      bundling: {
-        minify: false,
-        keepNames: true,
-        externalModules: [
-          '@nestjs/microservices',
-          '@nestjs/websockets',
-          '@nestjs/platform-socket.io',
-          'cache-manager',
-          'class-transformer',
-          'class-validator',
-        ],
+    const cartApiLambda = new lambda.DockerImageFunction(
+      this,
+      'CartApiLambda',
+      {
+        code: lambda.DockerImageCode.fromImageAsset(
+          path.join(__dirname, '../..'),
+          { cmd: ['src/lambda.handler'] },
+        ),
+        memorySize: 512,
+        timeout: cdk.Duration.seconds(30),
       },
-    });
+    );
 
     const httpApi = new apigwv2.HttpApi(this, 'CartHttpApi', {
       apiName: 'cart-api',
